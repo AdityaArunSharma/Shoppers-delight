b237292726460df85c0177d9fa5ab42ace3916c7
Aditya Arun Sharma <chiragarunsharma@gmai.com>
2022-03-10 21:32:36 +0530

    section-6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
+import CartScreen from './Screens/CartScreen'
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 const App = ()=> {
@@ -15,6 +16,7 @@ const App = ()=> {
       
       <Route path='/' exact component={HomeScreen} />
       <Route path='/product/:id' component={ProductScreen}/>
+      <Route path='/cart/:id?' component={CartScreen}/>
       </Container>
     </main>
       
